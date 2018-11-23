278709a7d598ad9aa632c1edb2be32c32ce2c6cf
Rich <rich@rjb.local>
2018-11-23 10:47:01 +0000

    add minimal add app example

---
+++ examples/7_apps/svr.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('client'))
+
+app.get('/add', function (req, res) {
+ 
+    console.log(req.query);
+
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+    const result = a+b;
+
+    console.log( `Computing ${a} + ${b} = ${result}` );
+
+    res.json(result);
+
+});
+
+app.listen(8888);
