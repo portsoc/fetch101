04806f58bd2d1ca7a0765b23aede58bdb51a5ff7
Rich <rich@rjb.local>
2018-11-23 10:46:43 +0000

    add 'other people's data' example

---
+++ examples/6_other_peoples_data/script.js
@@ -0,0 +1,18 @@
+function pageLoaded() {
+	window.fetchit.addEventListener('click', doFetchIt);
+}
+
+async function doFetchIt() {
+	const response = await fetch('https://gist.githubusercontent.com/ear1grey/070571c34b914ee468e421aa28cbed26/raw/7aefbd8de4c746d5692890441f3e6ecf90168514/data.json');
+	const data = await response.json();
+
+    console.log(data);
+
+  for (const i of data) {
+    const li = document.createElement("li");
+    li.textContent = i;
+    document.getElementById("numberlist").appendChild(li);
+  }
+}
+
+window.addEventListener("load", pageLoaded);
