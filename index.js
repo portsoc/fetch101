affb5b4569633aa7f9a76cd1c90c92a8ca3e3e1a
Rich Boakes <rjb@port.ac.uk>
2019-10-14 10:52:33 +0100

    modify to lose logger

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 /*
  * This is index.js
- * Start by modifying the id, fn and sn functions to return
- * information about you, then open index.html to check what
- * else you have to do, adding functions to the end of this
- * file as necessary.
  *
  * NB: all code you write this year should use strict mode, so
  * we've enabled that by default with the first line of code.
  */
 
 'use strict';
-
-function id() {
-    return "Replace This With Your Student ID";
-    // e.g. return "UP654321";
-}
-
-function fn() {
-    return "Replace This With Your First Name";
-}
-
-function sn() {
-    return "Replace This With Your Surname";
-}
