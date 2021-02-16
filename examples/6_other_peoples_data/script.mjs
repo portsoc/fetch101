c08a8096a7c31c75f95769b34148d6558b055a0b
Rich Boakes <rjb@port.ac.uk>
2021-02-16 15:25:05 +0000

    use exception handling

--- examples/6_other_peoples_data/script.mjs
+++ examples/6_other_peoples_data/script.mjs
@@ -4,14 +4,18 @@ async function fetchData() {
   const url = 'https://gist.githubusercontent.com/ear1grey/070571c34b914ee468e421aa28cbed26/raw/7aefbd8de4c746d5692890441f3e6ecf90168514/data.json';
   const response = await fetch(url);
 
-  if (response.ok) {
-    const data = await response.json();
-    report(
-      data,
-      false,
-      '#numberlist',
-    );
-  } else {
+  try {
+    if (response.ok) {
+      const data = await response.json();
+      report(
+        data,
+        false,
+        '#numberlist',
+      );
+    } else {
+      throw new Error('connection failed');
+    }
+  } catch (error) {
     report(
       `${response.status} ${response.statusText} when loading ${url}`,
       true,
