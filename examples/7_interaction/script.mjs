c0c030cb9b3233921c6183a0cbc0ebdb8842ad6e
Matt Dennis <matthew.dennis@port.ac.uk>
2026-02-10 23:02:49 +0000

    changed example7 to use new server

--- examples/7_interaction/script.mjs
+++ examples/7_interaction/script.mjs
@@ -1,28 +1,34 @@
 async function checkWord() {
   const word = document.querySelector('#word');
   const result = document.querySelector('#result');
+  const message = document.querySelector('#message');
 
   if (word.value.length === 0) {
     result.textContent = 'Enter a word to check its validity.';
     return;
   }
 
-  const url = `https://dictionary-dot-gse-val.ey.r.appspot.com/${word.value}`;
+  const url = `https://webpro.drmatt.dev/words/${word.value}`;
   const response = await fetch(url);
-
+  const data = await response.json();
+  result.textContent = '';
+  message.textContent = '';
   switch (response.status) {
     case 200:
       result.textContent = word.value + ' is a valid word';
+      message.textContent = 'Definition: ';
       break;
     case 400:
-      result.textContent = word.value + ' is too short';
+      result.textContent = word.value + ' is invalid';
       break;
     case 404:
-      result.textContent = word.value + ' is not allowed';
+      result.textContent = word.value + ' is invalid';
       break;
     default:
       result.textContent = 'the word checking service seems not to be available at this time';
+      return;
   }
+  message.textContent += data.message;
 }
 
 function pageLoaded() {
